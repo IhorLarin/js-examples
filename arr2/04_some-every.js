c4ec60f561c69fe21f11ac0a2588d67bc6de25fd
Ihor Larin <rmlaring@gmail.com>
2018-03-19 01:01:04 +0200

    шота нове

--- arr2/04_some-every.js
+++ arr2/04_some-every.js
@@ -33,7 +33,24 @@ var res_4 = num_2.some(function (value) {
 
 console.log(res_4); // false
 
+console.log('\n');
 
+var s1 = ['js', 'apple', 'css'];
+
+var mySome = function (array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    if (callback(array[i], i, array)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var q = mySome(s1, function (value) {
+  return value === 'js'
+});
+
+console.log(q);
 
 
 
